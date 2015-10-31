6b45eb807ffdd83cb8f2766a953cbb2597951611
Nils Eriksson <Nilseriksson89.ne@gmail.com>
2015-10-31 15:51:54 +0100

    Add google analytics

--- popup.js
+++ popup.js
@@ -7,3 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }, false);
 }, false);
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-69515833-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButton(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+
+var buttons = document.querySelectorAll('button');
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', trackButtonClick);
+}
